67feb8f07c0f101e65ac05f255706785daadbab3
amboob223 <monk8896@gmail.com>
2023-08-19 07:47:44 -0400

    bet

--- server/server.js
+++ server/server.js
@@ -83,7 +83,7 @@ app.get("/gassers", async (req, res) => {
     }
 })
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT
 
 app.listen(PORT, () => {
     console.log("server works")
